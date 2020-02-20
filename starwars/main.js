f1f0f25f68c19504243bd6bf8ed81f7045c1371b
Abby Allan <abbyallan306@gmail.com>
2020-02-20 12:44:14 -0700

    added js to star wars page

---
+++ starwars/main.js
@@ -0,0 +1,11 @@
+import { films } from '../data/films.js'
+
+console.log ('Hi There! Welcome')
+
+//console.log (document.querySelector(".greeting"))
+
+const greetingDiv = document.querySelector(".greeting")
+
+greetingDiv.textContent = 'I just inserted text into a DOM element using mad skillzzz'
+
+console.log(greetingDiv)
