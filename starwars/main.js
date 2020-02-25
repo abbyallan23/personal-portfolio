172aca8fd836c92d477fa7dced1dfd51581f25d5
Abby Allan <abbyallan306@gmail.com>
2020-02-25 12:42:04 -0700

    added starwars images through js

--- starwars/main.js
+++ starwars/main.js
@@ -1,11 +1,36 @@
 import { films } from '../data/films.js'
+import { people } from '../data/people.js'
+import { starships } from '../data/starships.js'
+import { species } from '../data/species.js'
+import { planets } from '../data/planets.js'
 
-console.log ('Hi There! Welcome')
-
-//console.log (document.querySelector(".greeting"))
 
 const greetingDiv = document.querySelector(".greeting")
 
-greetingDiv.textContent = 'I just inserted text into a DOM element using mad skillzzz'
+const castList = document.createElement("ul")
 
-console.log(greetingDiv)
+let counter = 1
+
+
+people.forEach(person => {
+    const listItem1 = document.createElement("li") 
+    listItem1.textContent = person.name
+    castList.appendChild(listItem1)
+
+    let anchorWrap = document.createElement("a")
+    anchorWrap.href = "a"
+
+    let imageItem = document.createElement("img")
+    imageItem.src = `https://starwars-visualguide.com/assets/img/characters/${counter}.jpg`
+    //add some way to handle user clicks on the image
+    imageItem.addEventListener("click", () => {
+    })
+
+    anchorWrap.appendChild(imageItem)
+    greetingDiv.appendChild(anchorWrap)
+    counter++
+
+})
+
+greetingDiv.appendChild(castList)
+
