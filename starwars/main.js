3219e0e3c7809cf802f6b76d1976a8841ed5bab1
Abby Allan <abbyallan306@gmail.com>
2020-03-03 12:43:33 -0700

    added populate DOM method

--- starwars/main.js
+++ starwars/main.js
@@ -11,28 +11,53 @@ const femaleButton = document.querySelector('#femaleButton')
 const otherButton = document.querySelector('#otherButton')
 
 const maleCharacters = people.filter(person => person.gender === "male")
-console.log(maleCharacters)
+console.log(maleCharacters.length)
 
 const femaleCharacters = people.filter(person => person.gender === "female")
-console.log(femaleCharacters)
+console.log(femaleCharacters.length)
 
 const otherCharacters = people.filter(person => {
-    if (person.gender === "hermaphrodite" || person.gender === "n/a") {
+    if (person.gender === "hermaphrodite" || person.gender === "n/a" || person.gender === "none" ){
         return person
     }
 })
+console.log(otherCharacters.length)
 
-let counter = 1
+maleButton.addEventListener("click", (event) => {
+    populateDOM(maleCharacters)
+})
+
+femaleButton.addEventListener("click", (event) => {
+    populateDOM(femaleCharacters)
+})
+
+otherButton.addEventListener("click", (event) => {
+    populateDOM(otherCharacters)
+})
+
+function getCharNumber(url) {
+    let end = url.lastIndexOf('/')
+    let start = end - 2
+    if(url.charAt(start) === '/') {
+        start++
+    }
+    return url.slice(start, end)
+
+}
+
+//getCharNumber("https://swapi.co/api/people/1/")
 
 
-people.forEach(person => {
-
+function populateDOM(maleCharacters) {
+    maleCharacters.forEach(person => {
+        //need to extract number from person.url property
+let charNum = getCharNumber(person.url)
 
     let anchorWrap = document.createElement("a")
     anchorWrap.href = "#"
 
     let imageItem = document.createElement("img")
-    imageItem.src = `https://starwars-visualguide.com/assets/img/characters/${counter}.jpg`
+    imageItem.src = `https://starwars-visualguide.com/assets/img/characters/${charNum}.jpg`
 
     imageItem.addEventListener('error', (event) => {
         //console.log(`${event.type}; Loading image\m')
@@ -47,11 +72,8 @@ people.forEach(person => {
 
     anchorWrap.appendChild(imageItem)
     greetingDiv.appendChild(anchorWrap)
-    counter++
+    //counter++
 
 })
 
-maleButton.addEventListener("click", (event) => {
-    console.log("clicked on maleButton")
-})
-
+}
