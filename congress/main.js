47b2c07f28ed64ebd1f52314d210cf42048a1099
Abby Allan <abbyallan306@gmail.com>
2020-03-29 19:43:49 -0600

    senator js fixed middle name null

--- congress/main.js
+++ congress/main.js
@@ -6,8 +6,25 @@ const main = document.querySelector('main')
 
 function populateSenatorDiv() {
     senators.forEach(senator => {
-        console.log(`${senator.first_name} ${senator.last_name}`)
+        let senatorName = `${senator.first_name} ${senator.last_name}`
+        let nameParagraph = document.createElement('p')
+        nameParagraph.textContent = senatorName
+        senatorDiv.appendChild(nameParagraph)
+    })
+    main.appendChild(senatorDiv)
+}
+
+const filterSenators = (prop, value) => {
+    return senators.filter(senator => {
+        return senator[prop] === value
     })
 }
 
+const senatorNames = senators.map(senator => {
+    let middleName = senator.middle_name ? ` ${senator.middle_name} ` : ` `
+    return {
+        id: senator.id,
+        name: `${senator.first_name}${middleName}${senator.last_name}`
+    }
+})
 populateSenatorDiv()
