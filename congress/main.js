af8c6079b6e1355d855a89af2cd91ceb5b371e47
Abby Allan <abbyallan306@gmail.com>
2020-03-31 20:51:22 -0600

    code cleanup

--- congress/main.js
+++ congress/main.js
@@ -3,31 +3,6 @@ import {representatives} from '../data/representatives.js'
 
 const senatorDiv = document.querySelector('.senators')
 
-function populateSenatorDiv() {
-    senators.forEach(senator => {
-
-        let middleName = senator.middle_name ? ` ${senator.middle_name} ` : ` `
-        let senFigure = document.createElement('figure')
-        let figImg = document.createElement('img')
-        let figCaption = document.createElement('figCaption')
-
-        figImg.src = `https://www.govtrack.us/static/legislator-photos/${senator.govtrack_id}-200px.jpeg`
-        figCaption.textContent = `${senator.first_name}${middleName}${senator.last_name}`
-
-        senFigure.appendChild(figImg)
-        senFigure.appendChild(figCaption)
-
-        senatorDiv.appendChild(senFigure)
-    })
-    main.appendChild(senatorDiv)
-}
-
-const filterSenators = (prop, value) => {
-    return senators.filter(senator => {
-        return senator[prop] === value
-    })
-}
-
 const senatorNames = senators.map(senator => {
     let middleName = senator.middle_name ? ` ${senator.middle_name} ` : ` `
     return {
@@ -37,7 +12,27 @@ const senatorNames = senators.map(senator => {
     }
 })
 
-console.log(senatorNames)
-//https://www.govtrack.us/static/legislator-photos/412741-200px.jpeg
 
-populateSenatorDiv()
+function populateSenatorDiv(justSenatorNames) {
+   justSenatorNames.forEach(senator => {
+
+        let senFigure = document.createElement('figure')
+        let figImg = document.createElement('img')
+        let figCaption = document.createElement('figCaption')
+
+        figImg.src = senator.imgURL
+        figCaption.textContent = senator.name
+
+        senFigure.appendChild(figImg)
+        senFigure.appendChild(figCaption)
+        senatorDiv.appendChild(senFigure)
+    })
+}
+
+const filterSenators = (prop, value) => {
+    return senators.filter(senator => {
+        return senator[prop] === value
+    })
+}
+
+populateSenatorDiv(senatorNames)
